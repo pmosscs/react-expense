c76875757bc724a2aac41522049bcb5653993a14
Peyton Moss <pmoss94@gmail.com>
2022-11-08 13:21:03 -0700

    adding variables dynamically

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div className="App">
-      <h1>Im here!</h1>
       <ExpenseItem />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Connecting</h2>;
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.52;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
