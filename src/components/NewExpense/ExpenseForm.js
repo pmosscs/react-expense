8f1c0f790caa43b9a2c42698531f63016f8f9033
Peyton Moss <pmoss94@gmail.com>
2022-11-09 11:00:24 -0700

    moved form to new component

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "ExpenseForm.css";
+
+function ExpenseForm() {
+  return <form action=""></form>;
+}
+
+export default ExpenseForm;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,8 @@
 import React from "react";
+import "./NewExpense.css";
 
 function NewExpense() {
-  return (
-    <div className="new-expense">
-      <form></form>
-    </div>
-  );
+  return <div className="new-expense"></div>;
 }
 
 export default NewExpense;
