a3368f1394a4a115d2f893f6cfbe162efe7d2b43
Peyton Moss <pmoss94@gmail.com>
2022-11-09 10:55:37 -0700

    learning useState, adding form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseItem.css";
 import Expensedate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  console.log("expense item function running");
+
+  const clickHandler = () => {
+    setTitle("updated!");
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <Expensedate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewExpense() {
+  return (
+    <div className="new-expense">
+      <form></form>
+    </div>
+  );
+}
+
+export default NewExpense;
